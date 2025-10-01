365edb1caaa6c1e885829fe67fbbcf2e1904b361
theabab23333 <2047477146@qq.com>
2025-10-01 13:23:12 +0800

    feat(meme): 初始化模组并更新配置

---
+++ src/main/java/me/theabab2333/meme/MemeMod.java
@@ -0,0 +1,21 @@
+package me.theabab2333.meme;
+
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.neoforge.common.NeoForge;
+import net.neoforged.neoforge.event.server.ServerStartingEvent;
+
+@Mod(MemeMod.MODID)
+public class MemeMod {
+    public static final String MODID = "meme";
+    public MemeMod(IEventBus modEventBus, ModContainer modContainer) {
+        NeoForge.EVENT_BUS.register(this);
+    }
+
+    @SubscribeEvent
+    public void onServerStarting(ServerStartingEvent event) {
+
+    }
+}
